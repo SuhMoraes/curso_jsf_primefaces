7cc79d9f423c25dcf446c2794dff30013302faf5
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-10-25 21:59:34 -0300

    Database Configuration

---
+++ email/src/test/java/dev/suhailahmoraes/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.suhailahmoraes.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
